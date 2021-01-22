71ef8e2b0c17ff5751e7b312e0e7d7b1b7cb8fb0
JoshSevy <joshuasevy@outlook.com>
2021-01-22 12:46:05 -0700

    Add other dynamic inputs and validators for form

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -12,8 +12,20 @@ export class CardFormComponent implements OnInit {
       Validators.required,
       Validators.minLength(3),
     ]),
-
-
+    cardNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(16),
+      Validators.maxLength(16)
+    ]),
+    expiration: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(0[1-9]|1[0-2])\/\d{2}$/),
+    ]),
+    securityCode: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(3)
+    ])
   });
 
   constructor() {
@@ -21,6 +33,10 @@ export class CardFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log('Form was submitted');
   }
 
 }
